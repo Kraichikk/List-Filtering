4a227d0276e65d749d1ba538f00a803b0f8c62be
Kraichikk <108270237+Kraichikk@users.noreply.github.com>
2023-02-24 14:36:29 +0200

    Add project files.

---
+++ List Filtering/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List_Filtering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems)
+        {
+            List<int> result = new List<int>();
+
+            foreach (var c in listOfItems)
+            {
+                if (c is int)
+                {
+                    result.Add((int)c);
+                }
+            }
+
+            return result;
+        }
+    }
+}
